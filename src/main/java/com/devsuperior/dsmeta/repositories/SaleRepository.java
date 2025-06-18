ba3a7e08d29dbb61f456820017656fd329a4cfb5
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-06-18 20:35:49 -0300

    Create summary sales with sellers

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY obj.seller.name")
+    List<SaleSummaryDTO> getSaleSummary(LocalDate minDate, LocalDate maxDate);
 
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +24,29 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public List<SaleSummaryDTO> getSummary(String minDate, String maxDate){
+		LocalDate startDate;
+		LocalDate endDate;
+
+		if (maxDate == null) {
+			endDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		} else {
+			endDate = LocalDate.parse(maxDate);
+		}
+
+		if (minDate == null) {
+			startDate = endDate.minusYears(1L);
+		} else {
+			startDate = LocalDate.parse(minDate);
+		}
+
+		if (minDate == null && maxDate == null) {
+			endDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+			startDate = endDate.minusMonths(12L);
+		}
+
+		return repository.getSaleSummary(startDate, endDate);
 	}
 }
